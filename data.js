a478c51b3418d1d0ca9b3d739a5ff19ea278b526
Timur Sheketa <timtimur82@gmail.com>
2023-06-10 13:48:08 +0300

    init

---
+++ data.js
@@ -0,0 +1,22 @@
+// const dataInfo = `
+// [
+//  {
+//     "type": "general",
+//     "setup": "Why did the house go to the doctor?",
+//     "punchline": "It was having window panes.",
+//     "id": 332
+//  },
+//  {
+//     "type": "general",
+//     "setup": "Want to hear my pizza joke?",
+//     "punchline": "Never mind, it's too cheesy.",
+//     "id": 154
+//  },
+//  {
+//     "type": "general",
+//     "setup": "Do you know what the word 'was' was initially?",
+//     "punchline": "Before was was was was was is.",
+//     "id": 37
+//  }
+// ]
+// `
